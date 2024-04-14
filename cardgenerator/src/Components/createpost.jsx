e2025427419daf928745ead78f44b397aa8b67bf
shubhampanghal1 <shubhampanghal7945@gmail.com>
2024-04-14 12:22:31 +0530

    added functionality for taking input from create post

--- cardgenerator/src/Components/createpost.jsx
+++ cardgenerator/src/Components/createpost.jsx
@@ -1,4 +1,14 @@
+import { useRef } from "react";
+
 function CreatePost() {
+  let postName = useRef();
+  let postContent = useRef();
+  function handleName(event) {
+    postName = event.target.value;
+  }
+  function handleContent(event) {
+    postContent = event.target.value;
+  }
   return (
     <>
       <form className="createpost">
@@ -11,13 +21,19 @@ function CreatePost() {
             className="form-control"
             id="name"
             aria-describedby="emailHelp"
+            onChange={(event) => handleName(event)}
           />
         </div>
         <div className="mb-3">
           <label for="exampleInputPassword1" className="form-label">
             Content
           </label>
-          <input type="text" className="form-control" id="content" />
+          <input
+            type="text"
+            className="form-control"
+            id="content"
+            onChange={(event) => handleContent(event)}
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
